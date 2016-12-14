1b08184f319ed4c6135975ccce90ed3bb1a9dbe8
MiJeong Park <mijoy0909@ibs.re.kr>
2016-12-14 16:59:39 +0900

    modified file

--- OPI/Main_Scripts/info.js
+++ OPI/Main_Scripts/info.js
@@ -6,13 +6,13 @@ importPackage(Packages.java.lang);
 var info= PVUtil.getString(pvs[0]);
 
 if(info == "Main.opi") {
-widget.setPropertyValue("text", "*MJ. Park (mijoy0909@ibs.re.kr), Control group, AET    *Last modified : 2016/11/17");
+widget.setPropertyValue("text", "*MJ. Park (mijoy0909@ibs.re.kr), Control group, AET    *Last modified : 2016/11/30");
 
 } else if(info == "Subsystem/ECR-IS/ECR-IS_Main.opi") {
-widget.setPropertyValue("text", "*MJ. Park (mijoy0909@ibs.re.kr), Control Group, AET / MJ.Kim (mjkim@ibs.re.kr), RH Group, RI exp.   *Last modified : 2016/11/29");
+widget.setPropertyValue("text", "*MJ. Park (mijoy0909@ibs.re.kr), Control Group, AET / MJ.Kim (mjkim@ibs.re.kr), RH Group, RI exp.   *Last modified : 2016/12/14");
 
 } else if(info == "Subsystem/LEBT/LEBT_Main.opi")  {
-widget.setPropertyValue("text", "*MJ. Park (mijoy0909@ibs.re.kr), Control Group, AET / MJ.Kim (mjkim@ibs.re.kr), RH Group, RI exp.   *Last modified : 2016/11/29");
+widget.setPropertyValue("text", "*MJ. Park (mijoy0909@ibs.re.kr), Control Group, AET / MJ.Kim (mjkim@ibs.re.kr), RH Group, RI exp.   *Last modified : 2016/12/14");
 
 } else if(info == "Subsystem/LLRF/LLRF_Main.opi")  {
 widget.setPropertyValue("text", "*MJ. Park (mijoy0909@ibs.re.kr), Control Group, AET /  ??, RF Group, AET   *Last modified : 2016/11/17");
