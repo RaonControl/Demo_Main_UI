9a2196ab8fa70487b15ed533fe32c323af5e298b
MiJeong Park <mijoy0909@ibs.re.kr>
2017-02-24 09:42:08 +0900

    add images & modified opis

---
+++ OPI/Main_Scripts/Change_Vacuum_DB.js
@@ -0,0 +1,25 @@
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil); 
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil); 
+importPackage(Packages.org.eclipse.jface.dialogs);
+importPackage(Packages.org.csstudio.platform.data);
+importPackage(Packages.java.lang);
+
+var pvStr0 = PVUtil.getString(pvs[0]);
+if(pvStr0=="All")
+display.getWidget("db").setPropertyValue("opi_file", "Subsystem/Vacuum/[LC]Vacuum_Main_DB.opi");
+else if(pvStr0=="50kV")
+display.getWidget("db").setPropertyValue("opi_file", "Subsystem/Vacuum/[LC]Vacuum_50Kv_DB.opi");
+else if(pvStr0=="70kV")
+display.getWidget("db").setPropertyValue("opi_file", "Subsystem/Vacuum/[LC]Vacuum_70Kv_DB.opi");
+else if(pvStr0=="Ground")
+display.getWidget("db").setPropertyValue("opi_file", "Subsystem/Vacuum/[LC]Vacuum_Ground_DB.opi");
+else if(pvStr0=="LEBT")
+display.getWidget("db").setPropertyValue("opi_file", "Subsystem/Vacuum/[LC]Vacuum_LEBT2_DB.opi");
+else if(pvStr0=="RFQ")
+display.getWidget("db").setPropertyValue("opi_file", "Subsystem/Vacuum/[LC]Vacuum_RFQ_DB.opi");
+else if(pvStr0=="MEBT")
+display.getWidget("db").setPropertyValue("opi_file", "Subsystem/Vacuum/[LC]Vacuum_MEBT_DB.opi");
+else if(pvStr0=="QWR")
+display.getWidget("db").setPropertyValue("opi_file", "Subsystem/Vacuum/[LC]Vacuum_QWR_DB.opi");
+else
+display.getWidget("db").setPropertyValue("opi_file", "Subsystem/Vacuum/[LC]Vacuum_Main_DB.opi");
